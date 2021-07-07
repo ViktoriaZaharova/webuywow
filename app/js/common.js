ebb013f1613104ae101c2f8b7af21c219f30b118
Viktoria <zaharova181114@gmail.com>
2021-07-07 17:36:24 +0300

    media1

--- app/js/common.js
+++ app/js/common.js
@@ -1,1 +1,25 @@
 $('.select').select2();
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 992) {
+        $('.reviews-slider:not(.slick-initialized)').slick({
+            infinite: true,
+            slidesToShow: 2,
+            arrows: true,
+            prevArrow: '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt=""></button>',
+            nextArrow: '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt=""></button>',
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
+    } else {
+        $(".reviews-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
